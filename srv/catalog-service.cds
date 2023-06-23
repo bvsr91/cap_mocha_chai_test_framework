e934199de6f2b77244313cb8c54f72a214629c14
Srini Reddy <bvsrxs@gmail.com>
2023-06-23 14:04:25 +0000

    first commit

---
+++ srv/catalog-service.cds
@@ -0,0 +1,19 @@
+using {NorthWind as external} from './external/NorthWind.csn';
+using {so_capsrv as so_extsrv} from './external/so_capsrv';
+
+service CatalogService {
+
+    @readonly
+    entity Products    as projection on external.Products {
+        key ID,
+            Name,
+            Description,
+            ReleaseDate,
+            DiscontinuedDate,
+            Rating,
+            Price
+    };
+
+    @readonly
+    entity SalesOrders as projection on so_extsrv.SalesOrders;
+}
---
+++ srv/catalog-service.js
@@ -0,0 +1,18 @@
+const cds = require('@sap/cds');
+
+module.exports = cds.service.impl(async function() {
+	const { Products } = this.entities;
+	const service = await cds.connect.to('NorthWind');
+	this.on('READ', Products, request => {
+		return service.tx(request).run(request.query);
+	});
+	this.on("READ", "SalesOrders", async req => {
+        try {
+            const service = await cds.connect.to('so_capsrv');
+            let result = await service.tx(req).run(req.query);
+            return result;
+        } catch (error) {
+            req.error("500", error.message);
+        }
+    });
+});
